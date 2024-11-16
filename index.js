1bffaa80cfbc03fec64cfecadcc0c303ab53c791
Mangesh Dhok <dhokmangesh678@gmail.com>
2024-11-16 20:03:50 +0530

    Update index.js with frontend deployed url

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://book-store-frontend-hk9t.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
   })
